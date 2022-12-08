227fafd1159814a370541e67390e586c7b1929a6
Patryk Wojtyczek <pwojtyczek@qualtrics.com>
2022-12-08 12:10:40 +0100

    extract graph creation from the core algorithm

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from random_walks import dry_run
+
+# 1. Extract graph definition from core algorithm.
+# 2. Create animation:
+#   Animations using networkxx
+#   https://colab.research.google.com/drive/1ed0fAC6rUMcScJYxv0Kc0Ox-wDBf2FSR?usp=sharing#scrollTo=jgtW7fk5JFRD
+
+
 if __name__ == '__main__':
     print('Gonna be fun!')
+    dry_run()
--- random_walks.py
+++ random_walks.py
@@ -2,52 +2,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 
-graph = nx.karate_club_graph()
 
-nx.draw(graph)
-plt.show()
-
-A = nx.adjacency_matrix(graph).toarray()
-
-N = len(A)
-
-PI = np.ones((N, N))
-PI /= np.sum(PI, axis=1)
-
-j = np.random.randint(N)
-
-heatmap = np.zeros(N)
-
-alpha = 0.5
-
-
-for t in range(100):
+def dry_run():
     """
-    p(i, t) - probability that the walker occupies node i at time t
+    Assume undirected graph for now.
 
-    PI[i, j]:
-        - transition matrix of probabilities moving from node j to node i as time t
-        - columns must sum to 1
+    I remember that there were some weights that we had to set?
+    Where are they set? Look up in the paper and then the code.
+
+    :return: None
     """
-    p = PI @ PI[:, j]
+    graph = nx.karate_club_graph()
 
-    while j == (i := np.random.choice(range(N), p=p)):
-        continue
-    heatmap[i] += 1
-
-    print(f"{j} -> {i}")
-
-    div = 0
-    for l in range(N):
-        div += A[l, j] * np.exp(alpha * p[l])
-
-    for k in range(N):
-        PI[k, j] = (A[k, j] * np.exp(alpha * p[k])) / div
-
-    j = i
+    random_walk(graph)
 
 
-print(heatmap)
+def random_walk(graph: nx.Graph):
 
-nx.draw(graph, node_color=heatmap, cmap='hot')
-plt.show()
+    nx.draw(graph)
+    plt.show()
+
+    A = nx.adjacency_matrix(graph).toarray()
+
+    N = len(A)
+
+    PI = np.ones((N, N))
+    PI /= np.sum(PI, axis=1)
+
+    j = np.random.randint(N)
+
+    heatmap = np.zeros(N)
+
+    alpha = 0.5
+
+    for t in range(100):
+        """
+        p(i, t) - probability that the walker occupies node i at time t
+
+        PI[i, j]:
+            - transition matrix of probabilities moving from node j to node i as time t
+            - columns must sum to 1
+        """
+        p = PI @ PI[:, j]
+
+        while j == (i := np.random.choice(range(N), p=p)):
+            continue
+        heatmap[i] += 1
+
+        print(f"{j} -> {i}")
+
+        div = 0
+        for l in range(N):
+            div += A[l, j] * np.exp(alpha * p[l])
+
+        for k in range(N):
+            PI[k, j] = (A[k, j] * np.exp(alpha * p[k])) / div
+
+        j = i
+
+    print(heatmap)
+
+    nx.draw(graph, node_color=heatmap, cmap='hot')
+    plt.show()
